7d8e098cdbd546511357dbc7a50f79a62d23dc65
Samantha Cardoso <sassacardosoo@gmail.com>
2020-07-07 23:25:00 -0300

    inclusao do <iframe>

--- apod.js
+++ apod.js
@@ -7,15 +7,26 @@ req.addEventListener("load", function(){
         let resposta = JSON.parse(req.responseText);
         let date = document.querySelector("#date");
         let image = document.querySelector("#image");
+        let frame = document.querySelector("#frame");
         let title = document.querySelector("#title");
         let copyright = document.querySelector("#copyright");
         let explanation = document.querySelector("#explanation");        
     
         date.textContent = resposta.date;
-        image.src = resposta.url;
         title.textContent = resposta.title;
         copyright.textContent = resposta.copyright;
         explanation.textContent = resposta.explanation;
+
+        if (resposta.media_type == "image") {
+            image.src = resposta.url;
+            frame.classList.add("invisible");
+        } else {
+            frame.src = resposta.url;
+            image.classList.add("invisible")
+        }
+        image.classList.add("image");
+        frame.classList.add("frame")
+
     } else {
         alert ("Ocorreu um erro")
     }
--- searchDate.js
+++ searchDate.js
@@ -10,15 +10,27 @@ selectDate.addEventListener("submit", function(){
                 let respostaData = JSON.parse(reqDate.responseText);
                 let date = document.querySelector("#date");
                 let image = document.querySelector("#image");
+                let frame = document.querySelector("#frame");
                 let title = document.querySelector("#title");
                 let copyright = document.querySelector("#copyright");
                 let explanation = document.querySelector("#explanation");
 
                 date.textContent = respostaData.date;
-                image.src = respostaData.url;
                 title.textContent = respostaData.title;
                 copyright.textContent = respostaData.copyright;
                 explanation.textContent = respostaData.explanation;
+
+                if (respostaData.media_type == "image") {
+                    image.src = respostaData.url;
+                    frame.classList.add("invisible");
+                    image.classList.remove("invisible")
+                } else {
+                    frame.src = respostaData.url;
+                    image.classList.add("invisible")
+                    frame.classList.remove("invisible")
+                }
+                image.classList.add("image");
+                frame.classList.add("video")        
                 
             } else {
                 alert ("Ocorreu um erro")
